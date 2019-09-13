01d242df46a45a29d7b4d4c85ffdadcbe2d7d1b7
Onur <onur1992@gmail.com>
2019-09-13 10:28:41 +0300

    Some Initialization Code Added

---
+++ Localization/Assets/Editor/LocalizationEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LocalizationEditor : EditorWindow
+{
+    /// <summary>
+    /// Initializes the editor window.
+    /// </summary>
+    [MenuItem("Window/Localization Editor")]
+    private static void InitializeWindow()
+    {
+        LocalizationEditor window = (LocalizationEditor)GetWindow(typeof(LocalizationEditor));
+        window.Show();
+    }
+}
